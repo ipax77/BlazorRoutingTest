20099eec6aa961d518b705b11331906cddfc1c1a
ipax77 <ph.ilipp@web.de>
2023-10-23 21:08:14 +0200

    Server Auto sample

---
+++ ServerAuto/BlazorApp1.Client/TestRecord.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp1.Client;
+
+public record TestRecord
+{
+    public TestRecord() { }
+    public TestRecord(string? test1, string? test2)
+    {
+        Test1 = test1;
+        Test2 = test2;
+    }
+    public string? Test1 { get; set; }
+    public string? Test2 { get; set; }
+}
